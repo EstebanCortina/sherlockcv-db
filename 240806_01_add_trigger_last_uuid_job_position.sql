dbf019796d7774dec92405760e47f94231cb5d8c
Esteban Cortina <estebannsebastian@gmail.com>
2024-08-08 12:41:56 -0600

    Add trigger for set last_uuid variable after update on job_position

--- 240806_01_add_trigger_last_uuid_job_position.sql
+++ 240806_01_add_trigger_last_uuid_job_position.sql
@@ -1,4 +1,4 @@
-DROP TRIGGER IF EXISTS ai_job_position_uuid;
+DROP TRIGGER IF EXISTS ai_job_position_uuid_insert;
 CREATE TRIGGER ai_job_position_uuid AFTER INSERT ON job_position
 FOR EACH ROW BEGIN
     SET @last_uuid = NEW.id;
